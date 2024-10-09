a82514ba87a313ec8eb3a4ac52e9160403daa3b8
mesheni <bunin.ivan14@yandex.ru>
2024-10-09 17:13:49 +0300

    Изменения: - Добавил библиотеку HandyControls - Реализовал пользовательский интерфейс

--- FSearch/MainWindow.xaml.cs
+++ FSearch/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace FSearch
 {
@@ -19,6 +10,16 @@ namespace FSearch
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseFolder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
