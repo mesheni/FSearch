b3d24622979a68ea623f0b9867546380f55ae077
mesheni <bunin.ivan14@yandex.ru>
2024-10-09 16:26:33 +0300

    Добавьте файлы проекта.

---
+++ FSearch/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FSearch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
